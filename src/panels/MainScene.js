210959e39711ca1ed4a4c1cb09d69afc8e0b233c
mishkanya <yahamedvedenko303@gmail.com>
2020-04-29 20:33:25 +0300

    fuck....

--- src/panels/MainScene.js
+++ src/panels/MainScene.js
@@ -37,7 +37,7 @@ class MainScene extends React.Component {
     
         this.state = {
           userInfo : "",
-          activeView: 'mainMenu'
+          activeView: 'mainMenu',
         }
       }
 
