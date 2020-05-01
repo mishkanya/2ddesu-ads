0f1d8dd828b69b1b3c4948bc7cdd78b4cd4f0714
mishkanya <yahamedvedenko303@gmail.com>
2020-05-02 00:30:45 +0300

    changeLink 3

--- src/panels/stickers.js
+++ src/panels/stickers.js
@@ -208,7 +208,7 @@ class Stickers extends React.Component {
           }
           {this.state.isMobilVersion?
             <Div>
-              В мобильном приложениии ВКонтакте выдача стикеров не доступна, но вы можете получить их в <Link target="_new" href="https://vk.cc/atxASV">веб версии приложения</Link>
+              В мобильном приложениии ВКонтакте выдача стикеров не доступна, но вы можете получить их в <Link target="_new" href="https://vk.com/app7367088_372864591">веб версии приложения</Link>
             </Div>
           :
             <Card align="center" onClick={()=> (this.checkSub())}  >
